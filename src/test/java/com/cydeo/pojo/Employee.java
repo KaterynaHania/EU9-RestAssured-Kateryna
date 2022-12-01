a3aa4ce6405b444567c9a3cf426951f622ecda13
katerynahania <ekaterinademkovskaia@gmail.com>
2022-12-02 00:15:49 +0100

    day6

--- src/test/java/com/cydeo/pojo/Employee.java
+++ src/test/java/com/cydeo/pojo/Employee.java
@@ -1,4 +1,64 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Employee {
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    @JsonProperty("job_id")
+    private String jobId;
+
+    private int salary;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", jobId='" + jobId + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/test/java/com/cydeo/pojo/Region.java
+++ src/test/java/com/cydeo/pojo/Region.java
@@ -1,4 +1,50 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class Region {
+
+    //region_id
+    //if your jsonkey and variable name not matching, you can map it with jsonProperty
+    @JsonProperty("region_id")
+    private int rId;
+    @JsonProperty("region_name")
+    private String region_name;
+    @JsonProperty("links")
+    private List<Link> links;
+
+    public int getrId() {
+        return rId;
+    }
+
+    public void setrId(int rId) {
+        this.rId = rId;
+    }
+
+    public String getRegion_name() {
+        return region_name;
+    }
+
+    public void setRegion_name(String region_name) {
+        this.region_name = region_name;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Link> links) {
+        this.links = links;
+    }
+
+    @Override
+    public String toString() {
+        return "Region{" +
+                "rId=" + rId +
+                ", region_name='" + region_name + '\'' +
+                ", links=" + links +
+                '}';
+    }
 }
